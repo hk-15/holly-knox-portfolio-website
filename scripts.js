d0508f4a672f5329282a4bc9de686068541c4f8a
Holly Knox <hollyknox15@gmail.com>
2025-03-28 12:03:49 +0000

    Added JavaScript interactivity

--- scripts.js
+++ scripts.js
@@ -2,23 +2,42 @@
 const project1 = document.getElementById('project1');
 const headingHighlight1 = document.getElementById('heading-highlight1');
 const description1 = document.getElementById('description1');
+const img1 = document.getElementById('project-img1');
+const img2 = document.getElementById('project-img2');
 
 /* event handler functions */
 function highlightText(_mouseenter) {
-    headingHighlight1.style.width = "30em";
+    headingHighlight1.style.display = "block";
 };
 
 function unhighlightText(_mouseleave) {
-    headingHighlight1.style.width = "0";
+    headingHighlight1.style.display = "none";
 };
 
 function showDescription(_onclick) {
-    description1.hidden="false";
+    if (description1.style.display === "none") {
+        description1.style.display = "block";
+        img1.style.display = "block";
+        img2.style.display = "block";
+
+    } else {
+        description1.style.display = "none";
+        img1.style.display = "none";
+        img2.style.display = "none";
+
+    }
+};
+
+function changeColor(_onclick) {
+    project1.style.color = 'red';
 }
 
- /* DOM events */
- project1.addEventListener('mouseenter', highlightText);
+/* DOM events */
+project1.onclick = showDescription;
 
- project1.addEventListener('mouseleave', unhighlightText);
+project1.addEventListener('mouseenter', highlightText);
 
- project1.addEventListener('onclick', showDescription);
+project1.addEventListener('mouseleave', unhighlightText);
+
+
+
